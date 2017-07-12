e4125ce6682e47dcdc866e7e78493303ebe9c9a9
luccafort <luccafort@gmail.com>
2017-07-13 00:20:46 +0900

    :bug: Model情報の更新
    作成するModelのテーブル情報を生成時に指定し忘れていたため再度Modelを再生成

--- db/migrate/20170712152018_create_novels.rb
+++ db/migrate/20170712152018_create_novels.rb
@@ -1,6 +1,10 @@
 class CreateNovels < ActiveRecord::Migration[5.1]
   def change
     create_table :novels do |t|
+      t.string :title
+      t.string :author
+      t.datetime :created_at
+      t.datetime :updated_at
 
       t.timestamps
     end

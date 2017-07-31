ebf5082d18591554e89dbef47542270092af51e0
luccafort <luccafort@gmail.com>
2017-08-01 00:25:24 +0900

    リクエスト値から小説情報を登録

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -12,6 +12,12 @@ class NovelsController < ApplicationController
     # Novels新規作成
   end
 
+  def create
+    @novel = Novel.new(post_params)
+    @novel.save
+    redirect_to @novel
+  end
+
   def edit
     # Novels更新
   end
@@ -23,4 +29,10 @@ class NovelsController < ApplicationController
   def destroy
     # Novels削除(論理削除)
   end
+
+  # 必要な値のみを取得
+  private
+    def post_params
+      params.require(:novels).permit(:title)
+    end
 end

e06f3120e41ee7e02b754b4caad3cd3056d5ffb4
luccafort <luccafort@gmail.com>
2017-08-09 23:31:43 +0900

    モデルクラスに発行したリレーションを追加

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,3 +1,4 @@
 class Content < ActiveRecord::Base
+  belongs_to :novel
   enum status: [draft: 100, publish: 200, limited: 300, deleted: 400, private: 500]
 end
--- app/models/novel.rb
+++ app/models/novel.rb
@@ -1,3 +1,5 @@
 class Novel < ActiveRecord::Base
+  belongs_to :author
+  has_many :contents, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
 end

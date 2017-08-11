6d9074e120b2d383d2153f42f58ce129344b77e1
luccafort <muspellsheim1985@gmail.com>
2017-08-11 15:21:33 +0900

    ルーティングにAuthorsを登録
    比叡山延暦寺にてコミット

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     resources :contents
   end
 
+  resources :authors
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

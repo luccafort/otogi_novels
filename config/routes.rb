db38ce03955eda05dca7f7592759b04164784b4d
luccafort <muspellsheim1985@gmail.com>
2017-09-11 23:44:34 +0900

    2重でルーティング設定を行っていたため修正
    AS句以降がすでに定義済みだったため削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :accounts, :controllers => { :omniauth_callbacks => "accounts/omniauth_callbacks" }
 
   devise_scope :account do
-    get 'sign_in', :to => 'devise/session#new', :as => :new_account_session
-    get 'sign_out', :to => 'devise/session#destroy', :as => :destroy_account_session
+    get 'sign_in', :to => 'devise/session#new'
+    get 'sign_out', :to => 'devise/session#destroy'
   end
 
   resources :novels do

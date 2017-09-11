c0d2eb4167f21db47707decb862de98217940282
luccafort <muspellsheim1985@gmail.com>
2017-09-11 23:05:07 +0900

    Callbackメソッドを実装
    Twitter/FacebookのCallback URLの受け入れ先メソッドをコントローラに実装

--- app/controllers/accounts/omniauth_callbacks_controller.rb
+++ app/controllers/accounts/omniauth_callbacks_controller.rb
@@ -1,4 +1,27 @@
 
 class Accounts::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  # FacebookのCallback APIの受け先
+  def facebook
+    @account = Account.find_for_facebook_oauth(request.env["omniauth.auth"], current_account)
+    # アカウント情報が保存済みかどうか判定
+    if @account.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+      sign_in_and_redirect @account, :event => :authentication
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to_new_account_registration_url
+    end
+  end
 
+  # TwitterのCallback APIの受け先
+  def twitter
+    @account = Account.find_for_twitter_oauth(request.env["omniauth.auth"], current_account)
+    if @account.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Twitter"
+      sign_in_and_redirect @account, :event => :authentication
+    else
+      session["devise.twitter_data"] = request.env["omniauth.auth"]
+      redirect_to_new_account_registration_url
+    end
+  end
 end

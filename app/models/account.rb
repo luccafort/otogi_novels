14a78f703a49124a6f9fadf5d1e50a39f4894492
luccafort <muspellsheim1985@gmail.com>
2017-09-16 01:25:05 +0900

    ユーザ情報をセットする実装を追加
    Facebook認証後にリダイレクトする登録フォームからはユーザ名のみをPOST

--- app/models/account.rb
+++ app/models/account.rb
@@ -11,4 +11,18 @@ class Account < ApplicationRecord
     Account.where(provider: auth.provider, uid: auth.uid).first
   end
 
+  def self.new_with_session(params, session)
+    super.tap do |account|
+      if data = session["devise.facebook_daa"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        account.email = data["email"] if account.email.blank?
+      end
+
+      if data = session["devise.facebook_data"]
+        account.provider = data["provider"] if account.provider.blank?
+        account.uid      = data["uid"] if account.uid.blank?
+        account.password = Devise.friendly_token[0, 20] if account.password.blank?
+      end
+    end
+  end
+
 end

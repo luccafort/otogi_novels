c2b1b622a7b2b9f24aaeef4ec8ee15c5c4dfef8f
luccafort <muspellsheim1985@gmail.com>
2017-09-16 01:07:37 +0900

    バリデーションの追加
    入力必須、最大文字列長15文字、英数字のみを許可するバリデーションをname属性に設定

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,5 +4,6 @@ class Account < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable
-  validates :name, presence: true, length: {maximum: 15}, format: { with: /\A[a-z0-9-]+\z/i, message: "is must NOT contain any other characters than alphanumerics." }
+  validates :name, presence: true, length: {maximum: 15},
+            format: { with: /\A[a-z0-9-]+\z/i, message: "is must NOT contain any other characters than alphanumerics." }
 end

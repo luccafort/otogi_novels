54990fb9de68bb780e6f9defc16e44e35fd12cd6
luccafort <muspellsheim1985@gmail.com>
2017-09-23 21:16:17 +0900

    コンテンツのStatusに未記入状態を追加
    コンテンツが新規作成されていない状態をデフォルト値として設定
    Enum型にエントリが存在しない状態を追加

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,4 +1,4 @@
 class Content < ActiveRecord::Base
   belongs_to :novel
-  enum status: [draft: 100, publish: 200, limited: 300, deleted: 400, private: 500]
+  enum status: [no_entry: 0, draft: 100, publish: 200, limited: 300, deleted: 400, private: 500]
 end

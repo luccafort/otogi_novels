ee77f469d61ad37a999ccf8f47382a800e97fe16
luccafort <muspellsheim1985@gmail.com>
2017-09-25 23:56:15 +0900

    Relation関係の解除
    Novelsモデルを新規作成するときにAuthorsモデルが必須になる条件を削除

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,2 @@
 class Author < ActiveRecord::Base
-  has_many :novels, dependent: :destroy
 end
--- app/models/novel.rb
+++ app/models/novel.rb
@@ -1,5 +1,4 @@
 class Novel < ActiveRecord::Base
-  belongs_to :author
   has_many :contents, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
 end

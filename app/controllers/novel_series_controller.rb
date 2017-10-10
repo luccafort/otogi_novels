d7106df3a70741d532b02d420a2856dc63b04968
luccafort <muspellsheim1985@gmail.com>
2017-10-10 22:34:52 +0900

    変数名やヘルパー関数名の修正

--- app/controllers/novel_series_controller.rb
+++ app/controllers/novel_series_controller.rb
@@ -1,23 +1,23 @@
 class NovelSeriesController < ApplicationController
   def index
     # Novelsの一覧表示
-    @novels = NovelSeries.all
+    @novel_series = NovelSeries.all
   end
 
   def show
     # 選択されたNovels詳細表示
-    @novel = NovelSeries.find(params[:id])
+    @novel_series = NovelSeries.find(params[:id])
   end
 
   def new
     # Novels新規作成
-    @novel = NovelSeries.new
+    @novel_series = NovelSeries.new
   end
 
   def create
-    @novel = NovelSeries.new(post_params)
-    if @novel.save
-      redirect_to @novel
+    @novel_series = NovelSeries.new(post_params)
+    if @novel_series.save
+      redirect_to @novel_series
     else
       # newメソッドをHTTP Code422で表示
       render :new, status: :unprocessable_entity
@@ -26,14 +26,14 @@ class NovelSeriesController < ApplicationController
 
   def edit
     # Novels更新
-    @novel = NovelSeries.find(params[:id])
+    @novel_series = NovelSeries.find(params[:id])
   end
 
   def update
     # Novels更新
-    @novel = NovelSeries.find(params[:id])
-    if @novel.update(post_params)
-      redirect_to @novel
+    @novel_series = NovelSeries.find(params[:id])
+    if @novel_series.update(post_params)
+      redirect_to @novel_series
     else
       render :edit, status: :unprocessable_entity
     end
@@ -41,8 +41,8 @@ class NovelSeriesController < ApplicationController
 
   def destroy
     # TODO: Novels削除(論理削除)
-    @novel = NovelSeries.find(params[:id])
-    @novel.destroy
+    @novel_series = NovelSeries.find(params[:id])
+    @novel_series.destroy
 
     # 削除後リダイレクト
     redirect_to novels_path
@@ -51,6 +51,6 @@ class NovelSeriesController < ApplicationController
   # 必要な値のみを取得
   private
     def post_params
-      params.require(:novel).permit(:title)
+      params.require(:novel_series).permit(:title)
     end
 end
--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,7 @@
 class StoriesController < ApplicationController
   # POST /novels/:novel_id/stories
   def create
-    @novelSeries = NovelSeries.find(params[:novel_id])
+    @novel_series = NovelSeries.find(params[:novel_id])
     @story = @novel.stories.create(get_story_params)
 
     redirect_to novel_path($novel)

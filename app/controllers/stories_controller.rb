2f98e27c89306a284ddda580b925d88e43624c8c
luccafort <muspellsheim1985@gmail.com>
2017-10-11 00:09:10 +0900

    モデル名を修正
    モデルのファイル名とクラス名は単数形でなければいけないため修正

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,8 +2,9 @@ class StoriesController < ApplicationController
   # POST /novels/:novel_id/stories
   def create
     @novel_series = NovelSeries.find(params[:novel_id])
-    @story = @novel.stories.create(get_story_params)
+    @story = @novel_series.story.create(get_story_params)
 
+    # TODO:パスの指定がおかしい正しくはnovel_series_path
     redirect_to novel_path($novel)
   end
 
--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,4 @@
-class Stories < ActiveRecord::Base
+class Story < ActiveRecord::Base
   belongs_to :novel
   enum status: [no_entry: 0, draft: 100, publish: 200, limited: 300, deleted: 400, private: 500]
 end

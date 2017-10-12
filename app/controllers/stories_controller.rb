14f1543582420a272690fb2eee7f4aca21b8339f
luccafort <muspellsheim1985@gmail.com>
2017-10-13 08:06:59 +0900

    Story一覧を用意

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,4 +1,10 @@
 class StoriesController < ApplicationController
+  def index
+    # タイトル表示にnovel_seriesのIDが必須
+    @novel_series = NovelSeries.find(get_novel_stories_params)
+    @stories = Stories.where(novel_series_id: get_novel_stories_params).order(:id)
+  end
+
   # POST /novels/:novel_id/stories
   def create
     @novel_series = NovelSeries.find(params[:novel_id])
@@ -10,5 +16,10 @@ class StoriesController < ApplicationController
   private
     def get_story_params
       params.require(:story).permit(:author, :body)
+    end
+
+  private
+    def get_novel_stories_params
+      params.require(:novel_series).permit(:id)
     end
 end

ec6561dfaedc5b971bc0f365279fea78095ee1e7
luccafort <muspellsheim1985@gmail.com>
2017-10-13 07:50:35 +0900

    Storyモデルのバリデーションに必須要素を追加

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,5 @@
 class Story < ActiveRecord::Base
   belongs_to :novel_series
   enum status: [no_entry: 0, draft: 100, publish: 200, limited: 300, deleted: 400, private: 500]
+  validates :content, presence: true, length: {minimum: 100, maximum: 10000}
 end

bc2ca52c465af0b52b930dbebefa3537e89e86c1
luccafort <muspellsheim1985@gmail.com>
2017-10-17 00:06:54 +0900

    不足しているRestfulなメソッドを仮実装
    あくまで仮実装なのでバリデーションや更新処理、トランザクションなどを実装する必要がある。

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -4,12 +4,44 @@ class StoriesController < ApplicationController
     @novel_series = NovelSeries.find(params[:novel_series_id])
   end
 
+  def show
+    # TODO:親であるところのnovel_seriesも取得する必要あり
+    # TODO:novel_seriesから著者名を取得する必要あり
+    @story = Story.find(params[:id])
+  end
+
+  def new
+    @story = Story.new
+  end
+
   # POST /novels/:novel_id/stories
   def create
     @novel_series = NovelSeries.find(params[:novel_series_id])
     @story = @novel_series.story.create(get_story_params)
 
     redirect_to novel_series_path($novel)
+  end
+
+  def edit
+    # 更新対象取得
+    @story = Story.find(parans[:id])
+  end
+
+  def update
+    # 更新
+    @story = Story.find(params[:id])
+    if @story.update(get_story_params)
+      redirect_to @story
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @story = Story.find(params[:id])
+    @story.destroy
+
+    redirect_to :controller => 'novel_series', :action => 'index'
   end
 
   private

7842e397156ab1ce12f4267d4e34fb4256931288
luccafort <muspellsheim1985@gmail.com>
2017-10-18 00:15:17 +0900

    validateのpresenceを削除
    accepts_nested_attributes_forをstoryモデルに追加したためpresence:trueがあると保存、更新が出来なくなるため削除

--- app/models/novel_series.rb
+++ app/models/novel_series.rb
@@ -1,4 +1,4 @@
 class NovelSeries < ActiveRecord::Base
   has_many :stories, dependent: :destroy
-  validates :title, presence: true, length: { minimum: 5 }
+  validates :title, length: { minimum: 5 }
 end

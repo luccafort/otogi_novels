c95733f0d22bdf812b0c89d74dca8367d3b31117
luccafort <muspellsheim1985@gmail.com>
2017-10-17 23:57:31 +0900

    accepts_nested_attributes_for属性の追加

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,6 @@
 class Story < ActiveRecord::Base
   belongs_to :novel_series
+  accepts_nested_attributes_for :novel_series
   enum status: [no_entry: 0, draft: 100, publish: 200, limited: 300, deleted: 400, private: 500]
   validates :content, presence: true, length: {minimum: 100, maximum: 10000}
 end
